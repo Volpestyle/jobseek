b335d702095a228f64a14ed62c3ddb5f06eea530
James Volpe <volpestyle@gmail.com>
2025-10-03 02:13:28 -0500

    chore: remove legacy Next.js app and APIs

--- app/dashboard/job-search/page.tsx
+++
@@ -1,5 +0,0 @@
-import { JobSearchPage } from "@/components/pages/JobSearchPage";
-
-export default function DashboardJobSearchPage() {
-  return <JobSearchPage />;
-}
--- app/dashboard/page.tsx
+++
@@ -1,5 +0,0 @@
-import { OverviewPage } from "@/components/pages/OverviewPage";
-
-export default function DashboardOverviewPage() {
-  return <OverviewPage />;
-}
